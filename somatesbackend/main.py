0dca49cdd795b030fcb31fef37f5e97e736501b7
Rohit kumar <128208098+RohitBuilds@users.noreply.github.com>
2026-04-04 10:53:20 +0530

    Refactor CORS middleware configuration
    Removed duplicate CORS middleware configuration and added it at the beginning for better organization.

--- somatesbackend/main.py
+++ somatesbackend/main.py
@@ -26,6 +26,14 @@ origins = [
     "https://somates-app.vercel.app",
 ]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],
+)
+
 app.include_router(signup_router)
 app.include_router(profile_router)
 app.include_router(follower_router)
@@ -44,10 +52,3 @@ app.include_router(privacy_router)
 def homeroute():
     return {'Welcome to somates app'}
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"],
-)
